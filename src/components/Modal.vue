e25028a447fe36fba16b02bab8c414244721401c
Maria Lucia Gubolin <mluciagubolin@gmal.com>
2022-04-25 15:59:31 -0300

    feat: Modal is opening and closing

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,10 +1,19 @@
 <template>
-
-    <div class="modal" >
+    <div class="modal">
         <div class="modal-background"></div>
         <div class="modal-content">
-           <slot></slot>
+            <div class="message is-info">
+                <div class="message-body">
+                    <slot></slot>
+                </div>
+            </div>
         </div>
-        <button class="modal-close is-large" aria-label="close"></button>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('fechaModal', false)"></button>
     </div>
 </template>
+
+<script lang='ts'>
+export default {
+    name: 'Modal',
+}
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,16 @@
 <script setup lang="ts">
 import Botao from '@/components/Botao.vue';
 import { ref } from 'vue';
+import Modal from '../components/Modal.vue';
 
 const mostraModal = ref(false);
-
 </script>
 
 <template>
     Sobre:
-    <Botao label="Info" class="is-info" @click="mostraModal=true"></Botao>
-
-
-    <div class="modal" :class="{'is-active' : mostraModal}">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-            <div class="message is-info">
-                <div class="message-body">
-            Algumas informações sobre o app??
-
-                </div>
-            </div>
-        </div>
-        <button class="modal-close is-large" aria-label="close" @click="mostraModal=false"></button>
-    </div>
-
+    <Botao label="Info" class="is-info" @click="mostraModal = true"></Botao>
+    <Modal
+        :class="{ 'is-active': mostraModal }"
+        @fechaModal="(setModal: boolean) => mostraModal = setModal"
+    >Algumas informações sobre o app??</Modal>
 </template>
